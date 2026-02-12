16caa9dd3addf9b0a387fa157c113e73190444ff
Poojaa <salagavapoojasri@gmail.com>
2026-02-12 10:20:45 +0530

    fixed gemini model name

--- gemini_service.py
+++ gemini_service.py
@@ -2,5 +2,5 @@ import google.generativeai as genai
 import os
 
 genai.configure(api_key=os.getenv("AIzaSyBwox9bH8iXKYCIGVmGukVplMw-nMKJupw"))
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
+model = genai.GenerativeModel("gemini-1.5-flash-001")
 
