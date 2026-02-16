6970fc2ebf84e578d472cb08ede562ac5fdb5fba
Poojaa <salagavapoojasri@gmail.com>
2026-02-16 11:42:57 +0530

    fixed gemini sdk

--- gemini_service.py
+++ gemini_service.py
@@ -4,9 +4,14 @@ from google import genai
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 def extract_from_image(image):
+
     response = client.models.generate_content(
-        model="gemini-2.0-flash",
-        contents=["Extract bill details", image],
+        model="gemini-2.5-flash",
+        contents=[
+            "Extract merchant, amount, date and category from this bill",
+            image
+        ],
     )
+
     return response.text
 
